23e9841ffbc288fb5696d6c1751b6c4f005db0a4
jdtherobot <jdtherobot@gmail.com>
2025-07-27 13:13:56 +0300

    Create two_sum.py

---
+++ easy/two_sum.py
@@ -0,0 +1,9 @@
+# Problem: Two Sum
+# Link: https://leetcode.com/problems/two-sum/
+# Author: JD
+
+def two_sum(nums, target):
+    for i in range(len(nums)):
+        for j in range(i + 1, len(nums)):
+            if nums[i] + nums[j] == target:
+                return [i, j]
